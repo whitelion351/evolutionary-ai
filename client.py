f4da3c3e92a7310b1f07c6175c4e7d7a673c164d
whitelion351 <56313776+whitelion351@users.noreply.github.com>
2021-04-01 13:52:17 -0400

    completed network code v1.0

--- client.py
+++ client.py
@@ -3,5 +3,5 @@ from evolutionai import AgentManager
 
 if __name__ == '__main__':
     manager = AgentManager(population=100, pool_size=0.1, network_layout=[8, 16, 16, 4], env_name="LunarLander-v2",
-                           continuous_action=False, episodes_per_agent=3, use_watchdog=500, watchdog_penalty=100,
-                           generations=500, obs_scale=1, action_scale=1, n_procs=2, is_client=True)
+                           continuous_action=False, episodes_per_agent=10, use_watchdog=500, watchdog_penalty=100,
+                           generations=500, obs_scale=1, action_scale=1, n_procs=4, is_client=True)
