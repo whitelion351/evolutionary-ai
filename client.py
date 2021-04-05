a9036a93c56ad5b712db5fca0b84448576b6f59d
whitelion351 <56313776+whitelion351@users.noreply.github.com>
2021-04-05 10:35:07 -0400

    v1.4b

--- client.py
+++ client.py
@@ -2,6 +2,6 @@ from evolutionai import AgentManager
 
 
 if __name__ == '__main__':
-    manager = AgentManager(population=100, pool_size=0.1, network_layout=[8, 16, 16, 4], env_name="LunarLander-v2",
-                           continuous_action=False, episodes_per_agent=10, use_watchdog=500, watchdog_penalty=100,
-                           generations=500, obs_scale=1, action_scale=1, n_procs=4, is_client=True)
+    manager = AgentManager(population=100, pool_size=0.1, network_layout=[8, 16, 16, 2], env_name="LunarLanderContinuous-v2",
+                           continuous_action=True, episodes_per_agent=10, generations=500, obs_scale=1, action_scale=1,
+                           n_procs=4, is_client=True)
