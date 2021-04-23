4162dd5c185df2a09806e9e5b4770849f92975ac
whitelion351 <56313776+whitelion351@users.noreply.github.com>
2021-04-23 00:26:47 -0400

    v1.5b

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
-from evolutionai import AgentManager
+from evolutionai import AgentManager, Agent
 
 
 if __name__ == '__main__':
-    manager = AgentManager(population=100, pool_size=0.1, network_layout=[8, 16, 16, 2], env_name="LunarLanderContinuous-v2",
-                           continuous_action=True, episodes_per_agent=10, generations=500, obs_scale=1, action_scale=1,
-                           n_procs=4, is_client=True)
+    manager = AgentManager(population=100, pool_size=0.1, network_layout=[24, 16, 16, 4], env_name="BipedalWalkerHardcore-v3",
+                           continuous_action=True, episodes_per_agent=10, generations=500, use_watchdog=300, watchdog_penalty=100, 
+                           obs_scale=1, action_scale=1, is_client=True,
+                           n_procs=4)
